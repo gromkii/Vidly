9779ec5f713348d8633b79eae9728f0bfd1fabff
Horrorbuns <limeside@gmail.com>
2017-09-21 11:25:35 -0500

    Add ViewModel for New Customer. :rabbit:

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,12 +9,18 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
+        [Display(Name = "Membership Type")]
         public MembershipType MembershipType { get; set; }
+
         public byte MembershipTypeId { get; set; }
+
         public DateTime? Birthday { get; set; }
     }
 }
